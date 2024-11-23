056ae09ba3690a2590bfa6af84be1ad7bcc2e534
muhammad <d20221101860@siswa.upsi.edu.my>
2024-11-23 16:51:55 +0800

    Update destination.php

--- destination.php
+++ destination.php
@@ -3,8 +3,23 @@ $title = 'Destination';
 include 'header.php';
 include 'db.php';
 
-$query = $conn->query("SELECT * FROM destinations");
-$destinations = $query->fetch_all(MYSQLI_ASSOC);
+// Get the selected state from the URL
+$state = isset($_GET['state']) ? $_GET['state'] : null;
+
+// Query data destinasi dari database
+if ($state) {
+    // Filter destinations by state
+    $stmt = $conn->prepare("SELECT * FROM destinations WHERE location = ?");
+    $stmt->bind_param("s", $state);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $destinations = $result->fetch_all(MYSQLI_ASSOC);
+    $stmt->close();
+} else {
+    // Show all destinations if no state is selected
+    $query = $conn->query("SELECT * FROM destinations");
+    $destinations = $query->fetch_all(MYSQLI_ASSOC);
+}
 ?>
 
 <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/bg_1.jpg');">
@@ -12,8 +27,13 @@ $destinations = $query->fetch_all(MYSQLI_ASSOC);
   <div class="container">
     <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
       <div class="col-md-9 ftco-animate pb-5 text-center">
-         <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="fa fa-chevron-right"></i></a></span> <span>Tour<i class="fa fa-chevron-right"></i></span></p>
-         <h1 class="mb-0 bread">Tour</h1>
+         <p class="breadcrumbs">
+            <span class="mr-2"><a href="index.php">Home <i class="fa fa-chevron-right"></i></a></span> 
+            <span>Tour <i class="fa fa-chevron-right"></i></span>
+         </p>
+         <h1 class="mb-0 bread">
+            <?php echo $state ? "Tour in " . htmlspecialchars($state) : "Tour"; ?>
+         </h1>
      </div>
     </div>
   </div>
@@ -34,21 +54,26 @@ $destinations = $query->fetch_all(MYSQLI_ASSOC);
 <section id="section-page1" class="ftco-section">
   <div class="container">
     <div class="row">
-      <?php foreach ($destinations as $destination): ?>
-        <div class="col-md-4 ftco-animate">
-          <div class="project-wrap">
-          <a href="destination-details.php?id=<?php echo $destination['id']; ?>" class="img" style="background-image: url('<?php echo $destination['image']; ?>');">
-          
-            </a>
-            <div class="text p-4">
-              <span class="days">Come Join Us</span>
-              <h3><a href="destination-details.php?id=<?php echo $destination['id']; ?>"><?php echo $destination['name']; ?></a></h3>
-              <p class="location"><span class="fa fa-map-marker"></span> <?php echo $destination['location']; ?></p>
-          
+      <?php if (!empty($destinations)): ?>
+        <?php foreach ($destinations as $destination): ?>
+          <div class="col-md-4 ftco-animate">
+            <div class="project-wrap">
+              <a href="destination-details.php?id=<?php echo $destination['id']; ?>" class="img" style="background-image: url('<?php echo htmlspecialchars($destination['image']); ?>');"></a>
+              <div class="text p-4">
+                <span class="days">Come Join Us</span>
+                <h3><a href="destination-details.php?id=<?php echo $destination['id']; ?>"><?php echo htmlspecialchars($destination['name']); ?></a></h3>
+                <p class="location"><span class="fa fa-map-marker"></span> <?php echo htmlspecialchars($destination['location']); ?></p>
+              </div>
             </div>
           </div>
+        <?php endforeach; ?>
+      <?php else: ?>
+        <div class="col-md-12">
+          <p class="text-center">
+            <?php echo $state ? "No destinations found for \"" . htmlspecialchars($state) . "\"." : "No destinations available at the moment. Please check back later!"; ?>
+          </p>
         </div>
-      <?php endforeach; ?>
+      <?php endif; ?>
     </div>
   </div>
 </section>
@@ -56,20 +81,14 @@ $destinations = $query->fetch_all(MYSQLI_ASSOC);
 <section id="section-page2" class="ftco-section" style="display: none;">
   <div class="container">
     <div class="row">
+      <!-- Placeholder for additional content (if required) -->
       <div class="col-md-4 ftco-animate">
         <div class="project-wrap">
-          <a href="destination-details.php" class="img" style="background-image: url(images/destination-2.jpg);">
-            <span class="price">$550/person</span>
-          </a>
+          <a href="destination-details.php" class="img" style="background-image: url(images/destination-2.jpg);"></a>
           <div class="text p-4">
             <span class="days">10 Days Tour</span>
             <h3><a href="destination-details.php">Banaue Rice Terraces</a></h3>
             <p class="location"><span class="fa fa-map-marker"></span> Banaue, Ifugao, Philippines</p>
-            <ul>
-              <li><span class="flaticon-shower"></span>2</li>
-              <li><span class="flaticon-king-size"></span>3</li>
-              <li><span class="flaticon-sun-umbrella"></span>Near Beach</li>
-            </ul>
           </div>
         </div>
       </div>
@@ -95,12 +114,14 @@ $destinations = $query->fetch_all(MYSQLI_ASSOC);
 </section>
 
 <!-- Include Chatbot -->
-<?php include 'chatbot.php';
+<?php 
+include 'chatbot.php';
 include 'footer.php';
 ?>
 
 <script>
   var currentPage = 1; // Track current page
+  const totalPages = 2; // Total number of pages
 
   function showPage(page) {
     if (page === 'page1') {
@@ -115,10 +136,9 @@ include 'footer.php';
   }
 
   function navigatePage(direction) {
-    if (direction === -1 && currentPage > 1) {
-      showPage('page' + (currentPage - 1));
-    } else if (direction === 1 && currentPage < 2) {
-      showPage('page' + (currentPage + 1));
+    const newPage = currentPage + direction;
+    if (newPage >= 1 && newPage <= totalPages) {
+      showPage('page' + newPage);
     }
   }
 </script>
